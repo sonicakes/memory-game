ccb5146b60b6448a894add61cacf7defb1c783b7
sonia parker <sparker@Sonias-MacBook-Air.local>
2018-04-25 17:09:38 +1000

    created game logic

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 console.log("Up and running!");
-
 var cards = [
 {
 rank: "queen",
@@ -30,14 +29,24 @@ function checkForMatch(){
             alert("Try again, sorry");
         }
 }
-function flipCard(cardId){
+function flipCard(){
+    var cardId = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
     cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute("src", cards[cardId].cardImage);
     if(cardsInPlay.length === 2){
    checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+         cardElement.addEventListener("click", flipCard);
+         document.getElementById("game-board").appendChild(cardElement);
+    }
+}
+createBoard();
